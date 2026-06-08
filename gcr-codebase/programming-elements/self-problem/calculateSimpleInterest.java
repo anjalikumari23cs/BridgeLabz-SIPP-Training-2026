a13477f73cd8f8cda0e977db4c69f3af0af555c0
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-06-08 12:45:40 +0530

    Anjali Kumari Add:Self Problem 1

---
+++ gcr-codebase/programming-elements/self-problem/calculateSimpleInterest.java
@@ -0,0 +1,9 @@
+public class calculateSimpleInterest {
+    public static void main(String[] args) {
+        double principle=800;
+        double rate=4;
+        double time=2;
+        double simpleInterest=(principle*rate*time)/100;
+        System.out.println("Simple Interest:"+simpleInterest);
+    }
+}
