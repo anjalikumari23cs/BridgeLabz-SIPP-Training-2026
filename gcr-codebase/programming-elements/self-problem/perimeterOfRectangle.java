4499a224b07d34a65824c709e27d9cfcb370c2c5
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-06-08 12:48:22 +0530

    Anjali Kumari Add:Self Problem 2

---
+++ gcr-codebase/programming-elements/self-problem/perimeterOfRectangle.java
@@ -0,0 +1,9 @@
+
+public class perimeterOfRectangle {
+    public static void main(String[] args) {
+        double length=5.0;
+        double width=3.0;
+        double perimeter=2*(length+width);
+        System.out.println("Perimeter of Rectangle:"+perimeter);
+    }
+}
