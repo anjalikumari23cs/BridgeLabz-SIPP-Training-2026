61ad700be3ec7dfd4afdb89d01b9449eaefe2349
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-06-08 12:36:44 +0530

    Anjali Kumari Add:Practice Question 3

---
+++ gcr-codebase/programming-elements/Day1/calsiusToFahrenheit.java
@@ -0,0 +1,7 @@
+public class calsiusToFahrenheit {
+    public static void main(String[] args) {
+        double celsius=37.0;
+        double fahrenheit=(celsius*9/5)+32;
+        System.out.println("Temperature in Fahrenheit:"+fahrenheit);
+    }
+}
