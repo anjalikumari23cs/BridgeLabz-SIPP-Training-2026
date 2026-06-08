20f9f115ee750d0cbf8d086fb3adc9d5a3655bab
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-06-08 12:40:45 +0530

    Anjali Kumari Add:Practice Question 5

---
+++ gcr-codebase/programming-elements/Day1/volumeOfCylinder.java
@@ -0,0 +1,8 @@
+public class volumeOfCylinder {
+    public static void main(String[] args) {
+        double radius=5.0;
+        double height=10.0;
+        double volume=Math.PI*radius*radius*height;
+        System.out.println("Volume of Cylinder:"+volume);
+    }
+}
