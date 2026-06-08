21c44cb230f9c77ac46db2ed1311c388a27d6ffd
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-06-08 12:54:33 +0530

    Anjali Kumari Add:Self Problem 5

---
+++ gcr-codebase/programming-elements/self-problem/convertKilometerToMiles.java
@@ -0,0 +1,7 @@
+public class convertKilometerToMiles {
+    public static void main(String[] args) {
+        double kilometers=5.0;
+        double miles=kilometers*0.621371;
+        System.out.println("Distance in Miles:"+miles);
+    }
+}
