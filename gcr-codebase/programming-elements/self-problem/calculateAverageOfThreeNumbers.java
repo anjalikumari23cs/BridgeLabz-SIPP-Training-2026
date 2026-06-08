1318bf9223c15783dbe9332196552f72b7517fcf
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-06-08 12:52:44 +0530

    Anjali Kumari Add:Self problem 4

---
+++ gcr-codebase/programming-elements/self-problem/calculateAverageOfThreeNumbers.java
@@ -0,0 +1,10 @@
+
+public class calculateAverageOfThreeNumbers {
+    public static void main(String[] args) {
+        double num1=10.0;
+        double num2=20.0;
+        double num3=30.0;
+        double average=(num1+num2+num3)/3;
+        System.out.println("Average of Three Numbers:"+average);
+    }
+}
