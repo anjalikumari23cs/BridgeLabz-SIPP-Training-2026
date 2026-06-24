9a81d0c2fc709cbd995162e4f0a30c727873a9ca
anjalikumari23cs <anjalikumari93680@gmail.com>
2026-06-24 09:30:43 +0530

    Anjali Modified:online retail System

--- oops-practice/Inheritance/Online Retail Order Management/OnlineRatailOrderManagment.java
+++ oops-practice/Inheritance/Online Retail Order Management/OnlineRatailOrderManagment.java
@@ -1,20 +1,3 @@
-// 3. Online Retail Order Management
-
-// ○ Description: Create a multilevel hierarchy to manage orders,
-// where Order is the base class, ShippedOrder is a subclass, and
-// DeliveredOrder extends ShippedOrder.
-// ○ Tasks:
-// ■ Define a base class Order with common attributes like
-// orderId and orderDate.
-// ■ Create a subclass ShippedOrder with additional attributes
-// like trackingNumber.
-// ■ Create another subclass DeliveredOrder extending
-// ShippedOrder, adding a deliveryDate attribute.
-// ■ Implement a method getOrderStatus() to return the
-// current order status based on the class level.
-
-// ○ Goal: Explore multilevel inheritance, showing how attributes
-// and methods can be added across a chain of classes.
 class Order {
     private String orderId;
     private String orderDate;
